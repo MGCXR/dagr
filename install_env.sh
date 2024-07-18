7af54a59c59e59ab211762b4ea63a02171084719
mlc <mlc@123>
2024-07-18 21:12:56 +0800

    1

--- install_env.sh
+++ install_env.sh
@@ -1,9 +1,9 @@
 #! /usr/bin/env bash
 
-TORCH=$(python -c "import torch; print(torch.__version__)")
-CUDA=$(python -c "import torch; print(torch.version.cuda)")
-URL=https://data.pyg.org/whl/torch-${TORCH}+${CUDA}.html
-
+# TORCH=$(python -c "import torch; print(torch.__version__)")
+# CUDA=$(python -c "import torch; print(torch.version.cuda)")
+# URL=https://data.pyg.org/whl/torch-${TORCH}+${CUDA}.html
+URL=https://data.pyg.org/whl/torch-2.5.0+124.html
 pip install --no-cache-dir torch-scatter -f $URL;
 pip install --no-cache-dir torch-cluster -f $URL;
 pip install --no-cache-dir torch-spline-conv -f $URL;
